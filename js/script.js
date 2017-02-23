61382d0efc1a15ae8a24cbaa30077da0317d7551
msilon <willardan@gmail.com>
2017-02-23 23:45:57 +0100

    Zadanie siodme

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = ('Zielone słonie').toUpperCase();
+var textReplace = text.replace('Papugi', animal);
+
+console.log(textReplace.substr(0, textReplace.length/2));
