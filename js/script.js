5459d6e2cf17f068dd42a7278dbb0aa5adb3d590
msilon <willardan@gmail.com>
2017-03-06 00:46:06 +0100

    choinka

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-var buttons = document.getElementsByClassName('button');
-
-for(var i=0; i < buttons.length; i++) {
-	var buttonsInnerText = buttons[i].innerText;
-	alert(buttonsInnerText);
+function rysujChoinke(rozmiarChoinki) {
+    for(var n=1; n<=rozmiarChoinki; n++) {
+        var star="";
+        var space="     ";
+        for(var i = 1; i <= n*2 - 1; i++) {
+            star += "*";
+        }
+        for(var i = 1; i <= n; i++)  {
+            space = space.slice(0, -1);
+        }
+        console.log(space + star);
+    }
 }
+rysujChoinke(5);
